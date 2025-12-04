d02fbb62fd619c0611ca5bd60835d985b1489726
maxgruhlke <maxgruhlke92@gmail.com>
2025-12-04 10:14:23 -0600

    Update Manager.h

--- BankManagementSystem/Manager.h
+++ BankManagementSystem/Manager.h
@@ -1,11 +1,22 @@
 #ifndef MANAGER_H
 #define MANAGER_H
 
+#include <string>
 #include "Person.h"
+using namespace std;
 
-//Manager can access all user data and transaction history
 class Manager : public Person {
+public:
+    Manager(string username, string password);
+    ~Manager(){}
 
+    static string findManager(string username);
+
+    bool isManager() const override {
+        return true;
+    }
+
+    // any manager-specific functions you plan to add later
 };
 
 #endif
