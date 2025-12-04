9ec87d866b11a49993a3b232900be351facb8ba0
maxgruhlke <maxgruhlke92@gmail.com>
2025-12-04 10:15:22 -0600

    Update Manager.cpp

--- BankManagementSystem/Manager.cpp
+++ BankManagementSystem/Manager.cpp
@@ -1,13 +1,34 @@
 
+#include "Manager.h"
 #include <iostream>
+#include <fstream>
 #include <string>
-#include "Person.h"
 using namespace std;
 
 //Manager can access all user data and transaction history
-class Manager : public Person{
-static int findUser(string username,string password)
-    {
-        return 0;//gives userid
+Manager::Manager(string username, string password) : Person(username, password){}
+
+string Manager::findManager(string username){
+    ifstream inputFile("managers.txt");
+    if(!inputFile.is_open()){
+        return "";
     }
-};
+
+    string line;
+    string fileUsername = "";
+    string filePassword = "";
+
+    while(getline(inputFile, line)){
+        if(line[0] == '!'){
+            fileUsername = line.erase(0,1);
+        }
+        else if(line[0] == '@'){
+            filePassword = line.erase(0, 1);
+            if(fileUsername == username){
+                return filePassword;
+            }
+        }
+    }
+
+    return "";
+}
