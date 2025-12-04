35771d99904920b96a2718064edc5a5487f1e2ee
maxgruhlke <maxgruhlke92@gmail.com>
2025-12-04 11:20:25 -0600

    Update Main.cpp

--- BankManagementSystem/Main.cpp
+++ BankManagementSystem/Main.cpp
@@ -22,18 +22,17 @@ return s;
 }
 
 string userLogin(){
-
     int option = 0;
-    int login = -1;
-    do{
+    while(true){
         string userLabel = "| Username: ";
         string passwordLabel = "| Password: ";
         string username = "";
         string password = "";
+
         cout << " " << setw(25) << setfill('_') << "" << endl;
         cout << setw(25) << left << setfill(' ') << "|" << "|" << endl;
         cout << setw(25) << left << "| Type back/exit anytime." << "|" << endl;
-        
+
         bool foundUsername = false;
         bool foundPassword = false;
         string currentUser = "";
@@ -45,15 +44,15 @@ string userLogin(){
             if(toLower(username) == "back"){
                 return "back";
             }
-            else if(toLower(username) == "exit"){
+            if(toLower(username) == "exit"){
                 return "exit";
             }
 
-            string testPassword = User::findUser(username);
+            string pass = User::findUser(username);
 
-            if(!testPassword.empty()){
-                currentUser = username;
+            if(!pass.empty()){
                 foundUsername = true;
+                currentUser = username;
             }
             else{
                 cout << "Username not found in system!" << endl;
@@ -67,14 +66,14 @@ string userLogin(){
 
             if(toLower(password) == "back"){
                 return "back";
-            } 
-            else if(toLower(password) == "exit"){
+            }
+            if(toLower(password) == "exit"){
                 return "exit";
             }
 
-            string updatedPassword = User::findUser(currentUser);
+            string correctPass = User::findUser(currentUser);
 
-            if((password == updatedPassword)){
+            if(password == correctPass){
                 foundPassword = true;
                 cout << "Successfully logged in!" << endl;
                 currentPerson = new User(currentUser, password);
@@ -85,16 +84,10 @@ string userLogin(){
 
         } while(!foundPassword);
 
-        cout << "|" << setw(19) << left << setfill('_') << "" << "|" << endl;
-        cin >> option;
+        cout << "|" << setw(25) << left << setfill('_') << "" << "|" << endl;
 
-        if(login != -1){
-            break;
-        }
-
-    } while(true);
-
-    return "bankAccount";
+        return "bankAccount";
+    }
 }
 
 void printAccount(int accountNum)
@@ -161,16 +154,16 @@ string createAccount(){
 
 string managerLogin(){
     int option = 0;
-    int login = -1;
-    do{
+    while(true){
         string userLabel = "| Manager Username: ";
         string passwordLabel = "| Manager Password: ";
         string username = "";
         string password = "";
+
         cout << " " << setw(25) << setfill('_') << "" << endl;
         cout << setw(25) << left << setfill(' ') << "|" << "|" << endl;
         cout << setw(25) << left << "| Type back/exit anytime." << "|" << endl;
-        
+
         bool foundUsername = false;
         bool foundPassword = false;
         string currentUser = "";
@@ -182,15 +175,15 @@ string managerLogin(){
             if(toLower(username) == "back"){
                 return "back";
             }
-            else if(toLower(username) == "exit"){
+            if(toLower(username) == "exit"){
                 return "exit";
             }
 
-            string testPassword = Manager::findManager(username);
+            string pass = Manager::findManager(username);
 
-            if(!testPassword.empty()){
-                currentUser = username;
+            if(!pass.empty()){
                 foundUsername = true;
+                currentUser = username;
             }
             else{
                 cout << "Username not found in system!" << endl;
@@ -204,14 +197,14 @@ string managerLogin(){
 
             if(toLower(password) == "back"){
                 return "back";
-            } 
-            else if(toLower(password) == "exit"){
+            }
+            if(toLower(password) == "exit"){
                 return "exit";
             }
 
-            string updatedPassword = Manager::findManager(currentUser);
+            string correctPass = Manager::findManager(currentUser);
 
-            if((password == updatedPassword)){
+            if(password == correctPass){
                 foundPassword = true;
                 cout << "Successfully logged in!" << endl;
                 currentPerson = new Manager(currentUser, password);
@@ -222,16 +215,61 @@ string managerLogin(){
 
         } while(!foundPassword);
 
-        cout << "|" << setw(19) << left << setfill('_') << "" << "|" << endl;
+        cout << "|" << setw(25) << left << setfill('_') << "" << "|" << endl;
+
+        return "managerMenu";
+    }
+}
+
+
+string managerMenu(){
+    int option = 0;
+
+    do{
+        cout << " " << setw(35) << setfill('_') << "" << endl;
+        cout << setw(35) << left << setfill(' ') << "|" << "|" << endl;
+        cout << setw(35) << left << "| Manager Menu" << "|" << endl;
+        cout << setw(35) << left << "| 1. View All Users" << "|" << endl;
+        cout << setw(35) << left << "| 2. View User Details" << "|" << endl;
+        cout << setw(35) << left << "| 3. View All Transactions" << "|" << endl;
+        cout << setw(35) << left << "| 4. View User Transactions" << "|" << endl;
+        cout << setw(35) << left << "| 5. Back" << "|" << endl;
+        cout << setw(35) << left << "| 6. Exit" << "|" << endl;
+        cout << "|" << setw(34) << left << setfill('_') << "" << "|" << endl;
+
         cin >> option;
+        
+        string inputted_username = "";
+        Manager* mgr = dynamic_cast<Manager*>(currentPerson);
 
-        if(login != -1){
-            break;
+        switch(option){
+            case 1:
+                mgr->viewAllUsers();
+                break;
+            case 2:
+                cout << "Enter username you wish to see credentials: ";
+                cin >> inputted_username;
+                mgr->viewUserDetails(inputted_username);
+                break;
+            case 3:
+                mgr->viewAllTransactions();
+                break;
+            case 4:
+                cout << "Enter username you wish to see transactions: ";
+                cin >> inputted_username;
+                mgr->viewUserTransactions(inputted_username);
+                break;
+            case 5:
+                return "back";
+            case 6:
+                return "exit";
+            default:
+                cout << "Invalid option" << endl;
+                break;
         }
-
     } while(true);
-
-    return "";
+    
+    return "managerMenu";
 }
 
 
@@ -279,7 +317,8 @@ void run()
         {"createAccount",createAccount},
         {"managerLogin",managerLogin},
         {"createBankAccount",createBankAccount},
-        {"transaction",transaction}
+        {"transaction",transaction},
+        {"managerMenu", managerMenu}
     };
     stack<string>pageStack;
     string currentPage="homePage";
@@ -308,4 +347,3 @@ int main(){
     run();
     delete currentPerson;
 }
-
