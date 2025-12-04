f0f9f8c33526d5846505b4f1e90599f9026138c4
maxgruhlke <maxgruhlke92@gmail.com>
2025-12-03 23:17:01 -0600

    Add files via upload

---
+++ BankManagementSystem/BankManagementSystem/User.cpp
@@ -0,0 +1,74 @@
+#include "User.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int User::numUsers = 0;
+
+User::User(string username, string password) : Person(username, password){
+    numAccounts = 0;
+    accounts = nullptr;
+}
+
+User::~User(){
+    if(accounts != nullptr){
+        delete[] accounts;
+    }
+}
+
+void User::loadExistingUsers(){
+    ifstream inputFile("users.txt");
+    if(!inputFile.is_open()){
+        return;
+    }
+
+    string line;
+    while(getline(inputFile, line)){
+        if(line[0] == '!'){
+            numUsers++;
+        }
+    }
+}
+
+string User::findUser(string username){
+    ifstream inputFile("users.txt");
+    if(!inputFile.is_open()){
+        return "";
+    }
+
+    string line;
+    string fileUsername = "";
+    string filePassword = "";
+
+    while(getline(inputFile, line)){
+        if(line[0] == '!'){
+            fileUsername = line.erase(0,1);
+        }
+        else if(line[0] == '@'){
+            filePassword = line.erase(0, 1);
+            if(fileUsername == username){
+                return filePassword;
+            }
+        }
+    }
+
+    return "";
+}
+
+bool User::createUser(string username, string password){
+    string exists = findUser(username);   
+    
+    if(exists.empty()){
+        User::numUsers++;
+        
+        ofstream outputFile("users.txt", ofstream::app);
+        if(outputFile.is_open()){
+            outputFile << "!" << username << "\n";
+            outputFile << "@" << password << "\n";
+        }
+        return true;
+    }
+
+    return false;
+}
