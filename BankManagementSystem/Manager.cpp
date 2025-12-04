495936a8c40be6402c109d1436d699a3e530d286
maxgruhlke <maxgruhlke92@gmail.com>
2025-12-04 11:19:26 -0600

    Update Manager.cpp

--- BankManagementSystem/Manager.cpp
+++ BankManagementSystem/Manager.cpp
@@ -32,3 +32,67 @@ string Manager::findManager(string username){
 
     return "";
 }
+
+void Manager::viewAllUsers(){
+    ifstream inputFile("users.txt");
+    if(!inputFile.is_open()){
+        return;
+    }
+
+    string line;
+    string fileUsername = "";
+    string filePassword = "";
+
+    cout << "===== All Users =====" << endl;
+
+    while(getline(inputFile, line)){
+        if(line[0] == '!'){
+            fileUsername = line.erase(0,1);
+        }
+        else if(line[0] == '@'){
+            filePassword = line.erase(0,1);
+            cout << "Username: " << fileUsername << endl;
+            cout << "Password: " << filePassword << endl;
+        }
+    }
+    cout << "=====================" << endl;
+    inputFile.close();
+}
+
+void Manager::viewUserDetails(string username){
+    ifstream inputFile("users.txt");
+    if(!inputFile.is_open()){
+        return;
+    }
+
+    string line;
+    string fileUsername = "";
+    string filePassword = "";
+
+    while(getline(inputFile, line)){
+        if(line[0] == '!'){
+            fileUsername = line.erase(0,1);
+        }
+        else if(line[0] == '@'){
+            filePassword = line.erase(0,1);
+            if(fileUsername == username){
+                cout << "===== User Details =====" << endl;
+                cout << "Username: " << fileUsername << endl;
+                cout << "Password: " << filePassword << endl;  
+                cout << "========================" << endl;
+                return;
+            }
+        }
+    }
+    inputFile.close();
+    cout << "User not found." << endl;
+}
+
+//FUNCTIONS NEEDED TO BE IMPLEMENTED
+void Manager::viewAllTransactions(){
+
+}
+
+void Manager::viewUserTransactions(string username){
+    
+}
