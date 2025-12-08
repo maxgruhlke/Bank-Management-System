ef9ff07cc544f6e5faefca85974a06d90c0b1928
maxgruhlke <maxgruhlke92@gmail.com>
2025-12-08 13:37:11 -0600

    Update Manager.cpp

--- BankManagementSystem/Manager.cpp
+++ BankManagementSystem/Manager.cpp
@@ -8,7 +8,7 @@ using namespace std;
 //Manager can access all user data and transaction history
 Manager::Manager(string username, string password) : Person(username, password){}
 
-string Manager::findManager(string username){
+string Manager::findPerson(string username){
     ifstream inputFile("managers.txt");
     if(!inputFile.is_open()){
         return "";
@@ -89,11 +89,73 @@ void Manager::viewUserDetails(string username){
     cout << "User not found." << endl;
 }
 
-//FUNCTIONS NEEDED TO BE IMPLEMENTED
-void Manager::viewAllTransactions(){
+void Manager::viewAllTransactions() {
+    ifstream inputFile("users.txt");
+    if(!inputFile.is_open()){
+        cout << "Unable to open users.txt" << endl;
+        return;
+    }
 
+    string line;
+    string username = "";
+
+    cout << "===== All Transactions =====" << endl;
+
+    while(getline(inputFile, line)){
+        if(line[0] == '!'){
+            username = line.erase(0, 1);
+        }
+        else if(line[0] == '@'){
+            string accountPath;
+            for(int accountID = 0; ; accountID++) {
+                accountPath = "Users/" + username + "/account" + to_string(accountID) + ".txt";
+                if(!filesystem::exists(accountPath)) break;
+
+                ifstream accountFile(accountPath);
+                if(accountFile.is_open()){
+                    string accountLine;
+                    cout << "User: " << username << ", Account " << accountID << " Transactions:" << endl;
+                    while(getline(accountFile, accountLine)){
+                        if(accountLine[0] == '#'){
+                            cout << accountLine << endl;
+                        }
+                    }
+                    accountFile.close();
+                    cout << "------------------------" << endl;
+                }
+            }
+        }
+    }
+
+    cout << "============================" << endl;
+    inputFile.close();
 }
 
 void Manager::viewUserTransactions(string username){
-    
+    string accountPath;
+    bool userExists = false;
+
+    for(int accountID = 0; ; accountID++) {
+        accountPath = "Users/" + username + "/account" + to_string(accountID) + ".txt";
+        if(!filesystem::exists(accountPath)) break;
+
+        userExists = true;
+
+        ifstream accountFile(accountPath);
+        if(accountFile.is_open()){
+            string accountLine;
+            cout << "User: " << username << ", Account " << accountID << " Transactions:" << endl;
+            while(getline(accountFile, accountLine)){
+                if(accountLine[0] == '#'){
+                    cout << accountLine << endl;
+                }
+            }
+            accountFile.close();
+            cout << "------------------------" << endl;
+        }
+    }
+
+    if(!userExists){
+        cout << "User not found or has no accounts." << endl;
+    }
 }
